278bdd3ef789854e8b617b4108be22ed43f50a58
moratillaj <moratillaj@email.com>
2019-10-13 18:16:03 +0200

    spring project start

---
+++ src/main/java/com/nio/webflux/SpringWebfluxNioPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.nio.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxNioPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxNioPerformanceApplication.class, args);
+	}
+
+}
